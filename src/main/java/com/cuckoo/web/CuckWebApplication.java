bf750f125c5689dbfe7915f398116e4989ad0ebb
tanmq <tanmq@appledeMacBook-Pro.local>
2017-02-26 14:51:58 +0800

    new project

---
+++ src/main/java/com/cuckoo/web/CuckWebApplication.java
@@ -0,0 +1,12 @@
+package com.cuckoo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuckWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuckWebApplication.class, args);
+	}
+}
